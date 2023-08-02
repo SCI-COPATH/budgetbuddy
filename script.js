4c3d0a849605de4f05727040cc1f244102d61077
jayasankar J P <scicopath01@gmail.com>
2023-08-02 21:23:01 +0530

    BAsic funsanality

--- script.js
+++ script.js
@@ -1,10 +1,84 @@
 
 let compines=[]
 let current
+let mode=0
 window.onload = function() {
-    registerMode()
-    removeEntry()
+    // localStorage.clear()
+    compines= JSON.parse(localStorage.getItem('allData'))!=null?JSON.parse(localStorage.getItem('allData')):compines;
+    current= JSON.parse(localStorage.getItem('current'))!=null?JSON.parse(localStorage.getItem('current')):current;
+    mode= JSON.parse(localStorage.getItem('mode'))!=null?JSON.parse(localStorage.getItem('mode')):mode;
+    console.log(compines)
+    console.log(current)
+    console.log(mode)
+    if(mode==0){
+        registerMode()
+        removeEntry()
+        hideCompiney()
+        
+        loadHome()                
+        // 
+
+    }else{
+        removeEntry()
+        loadEntry()
+
+        setelmentMode()
+
+    }
     // hideCompiney()
+}
+oruForm=document.getElementById('enterCompiny');
+oruForm.addEventListener("submit",(e) => {
+    e.preventDefault()
+    // console.log(inputObj.value)
+    register() 
+})
+function loadHome(){
+    let data=document.getElementById('compines')
+    let message
+    data.innerHTML=''
+    compines.forEach(element => {
+        if(element.total>=0){
+             message=`<div class="compinyStyle" onClick=selectOp(this)>
+                    <h4>${element.name}<h4>
+                    <div class=moneyGet>${element.total}</div>
+                    </div>`
+        }else{
+            message=`<div class="compinyStyle" onClick=selectOp(this)>
+            <h4>${element.name}<h4>
+            <div moneyGot>${element.total}</div>
+            </div>`
+        }
+        data.insertAdjacentHTML("afterbegin",message)
+    });
+}
+function loadEntry(){
+    let index=compines.map(x=>x.name).indexOf(current)
+        console.log(index)
+        document.getElementById('customer').innerHTML=`<button onclick="home()"><</button><h2>${current}</h2>`
+
+        if(compines[index].total>=0){
+            document.getElementById('result').innerHTML=`<div>You will get</div> <p class="moneyGet">${compines[index].total}</p>`
+        }else{
+            document.getElementById('result').innerHTML=`<div>You will got</div> <p class="moneyGot">${-1*compines[index].total}</p>`
+        }
+        let message
+        document.getElementById('Entry').innerHTML=''
+        compines[index].entry.forEach(element => {
+           
+            if(element.amount>=0){
+                message =`<div class='element_feature'>
+                           <div class="label">${element.remark}</div>
+                            <div class="amount moneyGet">${element.amount}</div>
+                         </div>`
+            }else{
+                message =`<div class='element_feature '>
+                            <div class="label">${element.remark}</div>
+                            <div class="amount moneyGot">${-1*element.amount}</div>
+                         </div>`
+            }
+            document.getElementById('Entry').insertAdjacentHTML("afterbegin",message)
+        });
 }
 function registerMode(){
     // document.getElementById('regster').style.display = "flex";
@@ -45,24 +119,32 @@ function register(){
                             <div>${compines.filter(x=>x.name==name).map(x=>x.total)}</div>
                          </div>`
                           
-            data.insertAdjacentHTML("beforeend",message)
+            data.insertAdjacentHTML("afterbegin",message)
             hideCompiney()
             document.getElementById('compinyName').value=''
+            localStorage.setItem('allData', JSON.stringify(compines))
+            localStorage.setItem('mode',JSON.stringify(mode))
+            localStorage.setItem('current',JSON.stringify(current))
         }
         
     }
 }
 function selectOp(data){
+    
+    mode=1
+    localStorage.setItem('mode',JSON.stringify(mode))
     current=data.querySelector('h4').innerText
+    localStorage.setItem('current',JSON.stringify(current))
     document.getElementById('customer').innerHTML=`<button onclick="home()"><</button><h2>${current}</h2>`
+    loadEntry()
     setelmentMode()
+    
     // hideCompiney()
     
 }
 function addEntry(){
     document.getElementById('entryDet').classList.add("EntryDet")
     document.getElementById('entryDet').classList.remove('hide')
-    console.log('clicking')
 }
 function removeEntry(){
     document.getElementById('entryDet').classList.remove("EntryDet")
@@ -92,16 +174,22 @@ function submitEntry(state){
                     <div class="amount moneyGot">${-1*amount}</div>
                  </div>`
     }
-    document.getElementById('Entry').insertAdjacentHTML("beforeend",message)
+    document.getElementById('Entry').insertAdjacentHTML("afterbegin",message)
     if(compines[index].total>=0){
         document.getElementById('result').innerHTML=`<div>You will get</div> <p class="moneyGet">${compines[index].total}</p>`
     }else{
         document.getElementById('result').innerHTML=`<div>You will got</div> <p class="moneyGot">${-1*compines[index].total}</p>`
     }
     removeEntry()
-    
+    localStorage.setItem('allData', JSON.stringify(compines))
+    localStorage.setItem('mode',JSON.stringify(mode))
+    localStorage.setItem('current',JSON.stringify(current))
+
 }
 function home(){
+    mode=0
+    localStorage.setItem('mode',JSON.stringify(mode))
+    loadHome()
     registerMode()
     removeEntry()
 }
